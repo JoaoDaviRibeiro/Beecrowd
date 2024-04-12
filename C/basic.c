b810bf0e74504a120edd3b590ed47ffc711000ae
João Davi Ribeiro <81536521+JoaoDaviRibeiro@users.noreply.github.com>
2024-04-11 21:00:53 -0300

    Create basic.c

---
+++ C/basic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int numUm;
+    int numDois;
+    
+    scanf("%d", &numUm);
+    scanf("%d", &numDois);
+    
+    int soma;
+    
+    soma = numUm + numDois;
+    
+    printf("X = %d", soma);
+ 
+    return 0;
+}
